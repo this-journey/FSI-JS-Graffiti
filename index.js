db370383ac322b669d94f58f1f6a398b5a004ac1
Demi Sherae Zayas <iMac@This-Journey.home>
2021-11-29 18:58:51 -0800

    Activity Changes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
